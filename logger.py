38517225eac4d77901280c5917f575ab8e4209ea
SanketMore <sanketmore@gmail.com>
2014-08-11 16:05:52 -0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,16 +3,23 @@ import datetime
 
 datetime = str(datetime.datetime.now())
 updatedfilename = 'shaketest_'+datetime+'.csv'
+line = []
 
-def addLine(data):
+def addToLine(data):
+    line.append.(data)
+
+
+def addLine:
     ''' simply logs data '''
 
-    if isinstance(data, list):
+    if isinstance(line, list):
         ofile = open(updatedfilename, "a")
         writer = csv.writer(ofile)
-        writer.writerow(data)
-    elif isinstance(data, float):
+        writer.writerow(line)
+    elif isinstance(line, float):
         with open(updatedfilename, "a") as myFile:
-            myFile.write(str(data)+'\n')
+            myFile.write(str(line)+'\n')
     else:
         print("No input for logger")
+
+    line = []
