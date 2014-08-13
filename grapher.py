bddd0920d77d33a140ed78d87fef3e495bf9d1fa
SanketMore <sanketmore@gmail.com>
2014-08-13 16:49:44 -0700

    Update grapher.py

--- grapher.py
+++ grapher.py
@@ -11,14 +11,20 @@ ax1 = fig.add_subplot(1,1,1)
 def animate(dataArray):
     pullData = os.popen('tail -n 4000 log.csv').read()
     dataArray = pullData.split('\n')
-    xar = []
-    yar = []
+    aR = []
+    bR = []
+    cR = []
+    dR = []
+    eR = []
+    fR = []
+    gR = []
     for eachLine in dataArray:
         if len(eachLine)>1:
-            y,x = eachLine.split(',')
-            xar.append(x)
-            yar.append(y)
-        
+            a,b,c,d,e,f,g = eachLine.split(',')
+            aR.append(x)
+            aR.append(x)
+            aR.append(x)
+            aR.append(x)
     ax1.clear()
     ax1.plot(xar,yar)
 
