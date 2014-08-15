9c7ff4585af0955d3061f88bc05fc4c89444deff
SanketMore <sanketmore@gmail.com>
2014-08-15 13:43:44 -0700

    Update grapher2.py

--- EXPERIMENTS/PI/grapher2.py
+++ EXPERIMENTS/PI/grapher2.py
@@ -9,71 +9,38 @@ import matplotlib.animation as animation
 
 
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-
-
-
-
-print('This program will display two data sets')
-
-specifyIntensity('First')
-typeA = getCharacterType()
-firstfilename = findFile(typeA)
-specifyIntensity('Second')
-typeB = getCharacterType()
-secondfilename = findFile(typeB)
-
-specifyWindow('First')
-windowA = getCharacterWindow()
-firstrowbegin, firstrowend = findWindow(windowA)
-specifyWindow('Second')
-windowB = getCharacterWindow()
-secondrowbegin, secondrowend = findWindow(windowB)
-
-specifyMeasurement('First')
-measurementA = getCharacterMeasurement()
-firstmeasurement = findMeasurement(measurementA)
-specifyMeasurement('Second')
-measurementB = getCharacterMeasurement()
-secondmeasurement = findMeasurement(measurementB)
-
-time1,value1 = findDataSet(firstfilename, firstrowbegin, firstrowend, firstmeasurement)
-time2,value2 = findDataSet(secondfilename, secondrowbegin, secondrowend, secondmeasurement)
-
-ani = animation.FuncAnimation(fig, animate(time1,value1,time2,value2), interval = 1000)
-plt.show()
-
-
 def findDataSet(filename, rowbegin, rowend, columnvalue):
     csvfile = open(filename, "rb")
     reader = csv.reader(csvfile)
     read = 0
     arrayTime= []
     arrayValue = []
+    test = 0
     for row in reader:
-        if row == rowbegin:
-            read = 1
         if row == rowend:
             read = 0
         if read == 1:
-            a,b,c,d,e,f,g = eachLine.split(',')
-            arrayTime.append(a)
-            if columnvalue == 1:
-                arrayValue.append(b)
-            elif columnvalue == 2:
-                arrayValue.append(c)
-            elif columnvalue == 3:
-                arrayValue.append(d)
-            elif columnvalue == 4:
-                arrayValue.append(e)
-            elif columnvalue == 5:
-                arrayValue.append(f)
-            elif columnvalue == 6:
-                arrayValue.append(g)
-            else:
-                print('Error')
-                sys.exit()
+            if len(row) >= columnvalue+1:
+                arrayTime.append(float(row[0]))
+                if columnvalue == 1:
+                    arrayValue.append(float(row[1]))
+                elif columnvalue == 2:
+                    arrayValue.append(float(row[2]))
+                elif columnvalue == 3:
+                    arrayValue.append(float(row[3]))
+                elif columnvalue == 4:
+                    arrayValue.append(float(row[4]))
+                elif columnvalue == 5:
+                    arrayValue.append(float(row[5]))
+                elif columnvalue == 6:
+                    arrayValue.append(float(row[6]))
+                else:
+                    print('Error')
+                    sys.exit()
+        if row == rowbegin:
+            read = 1
+	    print('Begin')
+
     return arrayTime, arrayValue
 
 
@@ -129,25 +96,25 @@ def findFile(value):
 
 def findWindow(value):
     if value == 'A':
-        return 'WINDOW=1', 'WINDOW=2'
+        return ['WINDOW=1'], ['WINDOW=2']
     elif value == 'B':
-        return 'WINDOW=2', 'WINDOW=5'
+        return ['WINDOW=2'], ['WINDOW=5']
     elif value == 'C':
-        return 'WINDOW=5', 'WINDOW=10'
+        return ['WINDOW=5'], ['WINDOW=10']
     elif value == 'D':
-        return 'WINDOW=10', 'WINDOW=20'
+        return ['WINDOW=10'], ['WINDOW=20']
     elif value == 'E':
-        return 'WINDOW=20', 'WINDOW=50'
+        return ['WINDOW=20'], ['WINDOW=50']
     elif value == 'F':
-        return 'WINDOW=50', 'WINDOW=100'
+        return ['WINDOW=50'], ['WINDOW=100']
     elif value == 'G':
-        return 'WINDOW=100', 'WINDOW=200'
+        return ['WINDOW=100'], ['WINDOW=200']
     elif value == 'H':
-        return 'WINDOW=200', 'WINDOW=500'
+        return ['WINDOW=200'], ['WINDOW=500']
     elif value == 'I':
-        return 'WINDOW=500', 'WINDOW=1000'
+        return ['WINDOW=500'], ['WINDOW=1000']
     elif value == 'J':
-        return 'WINDOW=1000', 'End'
+        return ['WINDOW=1000'], ['End']
     else:
         print('Error')
         sys.exit()
@@ -172,33 +139,90 @@ def findMeasurement(value):
 
 
 def getCharacterType():
-    character = sys.stdin.read(1)
+    character = raw_input()[0]
     if character in [ 'A','B', 'C', 'D', 'E', 'F']:
         print('Data Type Set')
         return character
     else:
         print('Invalid Input')
-        return getCharacter()
+        return getCharacterType()
 
 def getCharacterMeasurement():
-    character = sys.stdin.read(1)
+    character = raw_input()[0]
     if character in [ 'A','B', 'C', 'D', 'E', 'F']:
         print('Measurement Type Set')
         return character
     else:
         print('Invalid Input')
-        return getCharacter()
+        return getCharacterMeasurement()
           
 def getCharacterWindow():
-    character = sys.stdin.read(1)
+    character = raw_input()[0]
     if character in [ 'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']:
         print('WindowLength Set')
         return character
     else:
         print('Invalid Input')
-        return getCharacter()
+        return getCharacterWindow()
     
 
 def animate(time1,value1,time2,value2):
+    a = reduce(lambda x, y: x + y, value1) / len(value1)
+    b = reduce(lambda x, y: x + y, value2) / len(value2)
+    valueA = [x/a for x in value1]
+    valueB = [x/b for x in value2]
+    a = time1[0]
+    b = time2[0]
+    time1 = [x - a for x in time1]
+    time2 = [x - b for x in time2]
+
     ax1.clear()
-    ax1.plot(time1,value1, 'b', time2, value2, 'g')
+    ax2.clear()
+    ax1.plot(time1,value1,'b',time2, value2,'g')
+    
+
+
+
+
+
+#BEGIN
+
+
+
+fig1 = plt.figure()
+fig2 = plt.figure()
+ax1 = fig1.add_subplot(1,1,1)
+ax2 = fig2.add_subplot(1,1,1)
+
+
+
+
+print('This program will display two data sets')
+
+specifyIntensity('First')
+typeA = getCharacterType()
+firstfilename = findFile(typeA)
+
+specifyIntensity('Second')
+typeB = getCharacterType()
+secondfilename = findFile(typeB)
+
+specifyWindow('First')
+windowA = getCharacterWindow()
+firstrowbegin, firstrowend = findWindow(windowA)
+specifyWindow('Second')
+windowB = getCharacterWindow()
+secondrowbegin, secondrowend = findWindow(windowB)
+
+specifyMeasurement('First')
+measurementA = getCharacterMeasurement()
+firstmeasurement = findMeasurement(measurementA)
+specifyMeasurement('Second')
+measurementB = getCharacterMeasurement()
+secondmeasurement = findMeasurement(measurementB)
+
+time1,value1 = findDataSet(firstfilename, firstrowbegin, firstrowend, firstmeasurement)
+time2,value2 = findDataSet(secondfilename, secondrowbegin, secondrowend, secondmeasurement)
+
+animate(time1,value1,time2,value2)
+plt.show()
